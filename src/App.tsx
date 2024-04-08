9a9af49aa131afa7ff48b5fc639b2f63d2970c78
Artem <reebok001@yandex.ru>
2024-04-08 21:20:28 +0300

    finished the third lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { TaskType, TodoList } from "./components/Todolist/TodoList";
+import { v1 } from "uuid";
 
 export type FilterValuesType = "all" | "active" | "completed"
 
@@ -9,13 +10,24 @@ function App() {
     const todoListTitle = "What to learn"
 
     const [tasks, setTasks] = useState<Array<TaskType>>([
-        { id: 1, isDone: true, title: "HTML&CSS" },
-        { id: 2, isDone: true, title: "JS" },
-        { id: 3, isDone: false, title: "React" },
+        { id: v1(), isDone: true, title: "HTML&CSS" },
+        { id: v1(), isDone: true, title: "JS" },
+        { id: v1(), isDone: false, title: "React" },
     ])
 
-    const removeTask = (taskId: number) => {
+    const removeTask = (taskId: string) => {
         const newState = tasks.filter(t => t.id !== taskId)
+        setTasks(newState)
+    }
+
+    const addTask = (title: string) => {
+        const newTask: TaskType = {
+            id: v1(),
+            title,
+            isDone: false
+        }
+
+        const newState = [newTask, ...tasks]
         setTasks(newState)
     }
 
@@ -26,6 +38,7 @@ function App() {
                 title={ todoListTitle }
                 tasks={ tasks }
                 removeTask={ removeTask }
+                addTask={ addTask }
             />
         </div>
     );
--- src/components/Todolist/TodoListWithRef.tsx
+++ src/components/Todolist/TodoListWithRef.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useRef, useState, KeyboardEvent } from 'react';
+import React, { useRef, useState } from 'react';
 import './TodoList.css';
 import { FilterValuesType } from "../../App";
 
@@ -30,7 +30,7 @@ export const TodoList = ({
 
     const [filter, setFilter] = useState<FilterValuesType>("all")
 
-    const [taskTitle, setTaskTitle] = useState("")
+    const taskTitleInput = useRef<HTMLInputElement>(null)
 
     const getTasksForTodoList = (allTasks: Array<TaskType>, nextFilterValue: FilterValuesType) => {
         switch (nextFilterValue) {
@@ -44,37 +44,28 @@ export const TodoList = ({
     }
 
     const tasksForTodoList = getTasksForTodoList(tasks, filter)
-    const isTitleTooLong = taskTitle.length > 15
-    const ifTaskCanAdded = taskTitle && !isTitleTooLong
 
-    const tasksList: Array<JSX.Element> | JSX.Element = tasksForTodoList.length
-        ? tasksForTodoList.map(el => {
+    const tasksList: Array<JSX.Element> = tasksForTodoList.map(el => {
 
-                const onClickRemoveTaskHandler = () => removeTask(el.id)
+            const removeTaskHandler = () => removeTask(el.id)
 
-                return (
-                    <li key={ el.id }>
-                        <input type="checkbox" checked={ el.isDone } />
-                        <span>{ el.title }</span>
-                        <button onClick={ onClickRemoveTaskHandler }>x</button>
-                    </li>
-                )
-            }
-        )
-        : <div>Your tasks list is empty</div>
+            return (
+                <li key={ el.id }>
+                    <input type="checkbox" checked={ el.isDone } />
+                    <span>{ el.title }</span>
+                    <button onClick={ removeTaskHandler }>x</button>
+                </li>
+            )
+        }
+    )
 
     const onClickHandlerCreator = (filter: FilterValuesType) => () => setFilter(filter)
 
     const onClickAddTaskHandler = () => {
-        addTask(taskTitle)
-        setTaskTitle("")
-    }
-
-    const onChangeSetTaskTitle = (e: ChangeEvent<HTMLInputElement>) => setTaskTitle(e.currentTarget.value)
-
-    const onKeyDownAddTaskHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Enter" && ifTaskCanAdded) {
-            onClickAddTaskHandler()
+        if (taskTitleInput.current) {
+            const newTask = taskTitleInput.current.value
+            addTask(newTask)
+            taskTitleInput.current.value = ""
         }
     }
 
@@ -83,17 +74,8 @@ export const TodoList = ({
             <div className="todolist">
                 <h3>{ title }</h3>
                 <div>
-                    <input
-                        value={ taskTitle }
-                        onChange={ onChangeSetTaskTitle }
-                        onKeyDown={ onKeyDownAddTaskHandler }
-                    />
-                    <button
-                        disabled={ !ifTaskCanAdded }
-                        onClick={ onClickAddTaskHandler }
-                    >+
-                    </button>
-                    { isTitleTooLong && <div>Your task title is to long</div> }
+                    <input ref={ taskTitleInput } />
+                    <button onClick={ onClickAddTaskHandler }>+</button>
                 </div>
                 <ul>
                     { tasksList }
